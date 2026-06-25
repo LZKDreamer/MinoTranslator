0e80e017e5cdf6ff7333252861e23c63868b4c2a
Developer <dev@translator-plugin>
2026-06-25 22:19:41 +0800

    chore: bump release to v1.0.1

--- website/config/release.ts
+++ website/config/release.ts
@@ -1,7 +1,7 @@
 export const release = {
-  version: "1.0.0",
-  fileName: "Mino-Translator-v1.0.0.zip",
-  fileUrl: "https://github.com/LZKDreamer/MinoTranslator/releases/download/v1.0.0/Mino-Translator-v1.0.0.zip",
-  publishedAt: "2025-06-25",
+  version: "1.0.1",
+  fileName: "Mino-Translator-v1.0.1.zip",
+  fileUrl: "https://github.com/LZKDreamer/MinoTranslator/releases/download/v1.0.1/Mino-Translator-v1.0.1.zip",
+  publishedAt: "2026-06-25",
   chromeWebStoreStatus: "not_listed"
 } as const;
